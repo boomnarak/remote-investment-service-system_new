13aa69d2ad517fdf681022919bb7f64583650f41
BoomNaRaK <boomray248@gmail.com>
2024-09-27 14:36:45 +0700

    merge

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core'
-=======
-import { Component } from '@angular/core';
->>>>>>> parent of af2cc09 (config lint)
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
-<<<<<<< HEAD
 export class AppComponent implements OnInit {
-    ngOnInit(): void {
-        throw new Error('Method not implemented.')
-    }
-=======
-export class AppComponent {
-  title(title: any) {
-    throw new Error('Method not implemented.');
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.')
   }
->>>>>>> parent of af2cc09 (config lint)
 }
