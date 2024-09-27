1a948710f0a0ca3245b33b74f58419b548796b4d
BoomNaRaK <boomray248@gmail.com>
2024-09-27 14:35:25 +0700

    Revert "config lint"
    This reverts commit af2cc099b3791584a3657d126ac9d6203fbbced0.

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,12 +1,23 @@
+<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core'
+=======
+import { Component } from '@angular/core';
+>>>>>>> parent of af2cc09 (config lint)
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
+<<<<<<< HEAD
 export class AppComponent implements OnInit {
     ngOnInit(): void {
         throw new Error('Method not implemented.')
     }
+=======
+export class AppComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+>>>>>>> parent of af2cc09 (config lint)
 }
